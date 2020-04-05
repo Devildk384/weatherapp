24908d55997c662c9b3b25577cb4b4573e2ea3cb
Devildk384 <durgeshkumar384@gmail.com>
2020-04-05 15:03:14 +0530

    Updates

---
+++ precache-manifest.483369e57ea3385b6a7f8d6c0bb31ce6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340d29b95fe868bfab584d1e9580d1b2",
+    "url": "/Devildk384/appweather/index.html"
+  },
+  {
+    "revision": "2447cea8c2809c35c6ed",
+    "url": "/Devildk384/appweather/static/css/main.9be27119.chunk.css"
+  },
+  {
+    "revision": "64aa656e32717cfababb",
+    "url": "/Devildk384/appweather/static/js/2.7d5adff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Devildk384/appweather/static/js/2.7d5adff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2447cea8c2809c35c6ed",
+    "url": "/Devildk384/appweather/static/js/main.2c93cb00.chunk.js"
+  },
+  {
+    "revision": "e2eb8c17b056965058cf",
+    "url": "/Devildk384/appweather/static/js/runtime-main.070c9324.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Devildk384/appweather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Devildk384/appweather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
